2c471df6b1db26dcb6e30807653ac40511570e80
anthony gevers <anthony.gevers@lapiscine.pro>
2024-11-25 14:46:19 +0100

    template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController //Création de la classe HomeController
-
+class HomeController extends AbstractController
 {
+    #[Route('/', 'home')]
 
-    #[Route('/', 'home')]  //Création de la route pour la methode "home" le signe # est comme un commentaire lu et utilisé par PHP
-
-    public function home()  // Création de la fonction "home" qui retourne une instance de classe "Response" qui vient de Symfony
-//La classe permet de créer une reponse HTTP valide et prend en compte l'HTML de return a envoyer au navigateur.
+    public function home()
     {
-
-        return new Response( '<h1>Page d\'accueil</h1>');
+        return $this->render('base.html.twig', [
+            'title' => 'Bienvenue sur la page !',
+        ]);
     }
 }
