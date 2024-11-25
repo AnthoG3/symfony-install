f29386c1dd2fcfed8eb6f8c595a22b462dfcebdb
anthony gevers <anthony.gevers@lapiscine.pro>
2024-11-25 14:04:23 +0100

    commentaires de PokerController

--- src/Controller/PokerController.php
+++ src/Controller/PokerController.php
@@ -13,8 +13,11 @@ class PokerController
 
     {
 
+        //appelle la methode createFromGlobals sans avoir besoin de faire l'instance de classe manuellement
+        // Cette methode permet de remplir la variable $request avec toutes les données de requete (GET,POST,SESSION...)
         $request = Request::createFromGlobals();
 
+        //J'utilise la propriété query qui me permet de récuperer les données GET
         $age = $request->query->get('age');
 
         var_dump($age); die;
