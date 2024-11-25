ac55567aa5fce355e693bb3c2de02034cbd1bfff
anthony gevers <anthony.gevers@lapiscine.pro>
2024-11-25 15:48:42 +0100

    articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,47 +5,48 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController extends AbstractController
+class ArticleController extends AbstractController //Création de la classe "ArticleController" et AbstractControler
+// indique que ArticleController hérite de cette derniere qui permet d'utiliser "render"
 {
 
     #[Route('/articles', 'articles_list')]
-    public function articles()
+    public function articles()  //Définition de la ethode "articles"
     {
-        $articles = [
+        $articles = [ //Création d'un tableau qui contient des sous tableaux, chacuns representant un article avec "id,title,content,image"
             [
                 'id' => 1,
                 'title' => 'Article 1',
-                'content' => 'Content of article 1',
+                'content' => 'Youpi un premier article !!!!',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
             ],
             [
                 'id' => 2,
                 'title' => 'Article 2',
-                'content' => 'Content of article 2',
+                'content' => 'Génial un deuxieme article !!! ',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
             ],
             [
                 'id' => 3,
                 'title' => 'Article 3',
-                'content' => 'Content of article 3',
+                'content' => 'Oh un troisieme ! Super ! ',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
             ],
             [
                 'id' => 4,
                 'title' => 'Article 4',
-                'content' => 'Content of article 4',
+                'content' => 'Un quatrieme ? Mais quelle chance ! ',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
             ],
             [
                 'id' => 5,
                 'title' => 'Article 5',
-                'content' => 'Content of article 5',
+                'content' => 'On se fait chier là,non ?',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
             ]
 
         ];
 
-        return $this->render('articles_list.html.twig', [
+        return $this->render('articles_list.html.twig', [ //$this-> render est la methode hérité de "AbstractController" pour rendre une vue auquel on ajoute le nom du fichier Twig
             'articles' => $articles
         ]);
     }
