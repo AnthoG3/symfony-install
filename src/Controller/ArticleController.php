10d44eb2840494a9e0ecc1a4aff31740d4a2d826
anthony gevers <anthony.gevers@lapiscine.pro>
2024-11-26 17:04:55 +0100

    error 404

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
 
     #[Route('/articles', 'articles_list')]
-    public function articles()
+    public function articles(): Response
     {
 
         $articles = [
@@ -56,8 +57,8 @@ class ArticleController extends AbstractController
 
     }
 
-    #[Route('/article/{id}', 'article_show')]
-    public function showArticle($id)
+    #[Route('/article/{id}', 'article_show', ['id' => '\d+'])]
+    public function showArticle(int $id): Response
     {
 
         $articles = [
@@ -66,30 +67,35 @@ class ArticleController extends AbstractController
                 'title' => 'Article 1',
                 'content' => 'Content of article 1',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
             ],
             [
                 'id' => 2,
                 'title' => 'Article 2',
                 'content' => 'Content of article 2',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
             ],
             [
                 'id' => 3,
                 'title' => 'Article 3',
                 'content' => 'Content of article 3',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
             ],
             [
                 'id' => 4,
                 'title' => 'Article 4',
                 'content' => 'Content of article 4',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
             ],
             [
                 'id' => 5,
                 'title' => 'Article 5',
                 'content' => 'Content of article 5',
                 'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
             ]
 
         ];
@@ -97,7 +103,7 @@ class ArticleController extends AbstractController
         $articleFound = null;
 
         foreach ($articles as $article) {
-            if ($article['id'] === (int) $id) {
+            if ($article['id'] === $id) {
                 $articleFound = $article;
             }
         }
@@ -110,12 +116,7 @@ class ArticleController extends AbstractController
 
 
     #[Route('/articles/search-results', 'article_search_results')]
-    // plutôt d'instancier la classe Request manuellement
-        // je peux utiliser le système d'instanciation automatiquement de symfony pour ça, je lui passe en parametre de méthode
-        // le type de la classe voulue, suivie d'une variable dans laquelle je veux que symfony stocke l'instance de la classe
-        // ce mécanisme est appelé autowire
-    public function articleSearchResults(Request $request) {
-
+    public function articleSearchResults(Request $request): Response {
         $search = $request->query->get('search');
 
         return $this->render('article_search_results.html.twig', [
